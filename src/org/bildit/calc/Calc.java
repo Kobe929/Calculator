beab9eee75a9d56a3d8cb49a53577821dbaaf687
filip <filipdautovic@yahoo.com>
2016-11-16 09:54:39 +0100

    initial project setup

---
+++ src/org/bildit/calc/Calc.java
@@ -0,0 +1,34 @@
+package org.bildit.calc;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public static void addition(double a, double b){
+		
+	}
+	
+	public static void substraction(double a, double b){
+		
+	}
+	
+	public static void multiplication(double a, double b){
+		
+	}
+	
+	public static void division(double a, double b){
+		
+	}
+	
+	public static void squareRoot(double a){
+		
+	}
+	
+	public static void pow(double a){
+		
+	}
+
+}
