b153af973ad3bc55689f9e896029bf7d518c1e8e
nikola <nikola.lisicic@hotmail.com>
2016-11-16 10:57:39 +0100

    added squareRoot method

--- src/org/bildit/calc/Calc.java
+++ src/org/bildit/calc/Calc.java
@@ -3,32 +3,31 @@ package org.bildit.calc;
 public class Calc {
 
 	public static void main(String[] args) {
-		
 
 	}
-	
-	public static void addition(double a, double b){
-		
+
+	public static void addition(double a, double b) {
+
 	}
-	
-	public static void substraction(double a, double b){
-		
+
+	public static void substraction(double a, double b) {
+
 	}
-	
-	public static void multiplication(double a, double b){
-		
+
+	public static void multiplication(double a, double b) {
+
 	}
-	
-	public static void division(double a, double b){
-		
+
+	public static void division(double a, double b) {
+
 	}
-	
-	public static void squareRoot(double a){
-		
+
+	public static void squareRoot(double a) {
+		System.out.println(Math.sqrt(a));
 	}
-	
-	public static void pow(double a){
-		
+
+	public static void pow(double a) {
+
 	}
 
 }
